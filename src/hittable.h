34caf83941108baa7499b5b62289e70da0798ace
yada2x <lawrenceltnguyen@gmail.com>
2024-07-06 20:54:06 +1000

    Create rtweekends containing frequently used values and functions

--- src/hittable.h
+++ src/hittable.h
@@ -1,8 +1,6 @@
 #ifndef HITTABLE_H
 #define HITTABLE_H
 
-#include "ray.h"
-
 class hit_record {
     public:
         point3 p;
--- src/hittable_list.h
+++ src/hittable_list.h
@@ -3,11 +3,7 @@
 
 #include "hittable.h"
 
-#include <memory>
 #include <vector>
-
-using std::make_shared;
-using std::shared_ptr;
 
 class hittable_list : public hittable {
   public:
