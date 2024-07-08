cde108665a0305aaf40c3ce24d5654cfa274f839
yada2x <lawrenceltnguyen@gmail.com>
2024-07-08 10:38:06 +1000

    Move main code into camera class, fix bugs in interval.h

--- src/interval.h
+++ src/interval.h
@@ -2,28 +2,29 @@
 #define INTERVAL_H
 
 #include "rtweekend.h"
+const double infinity = std::numeric_limits<double>::infinity();
 
 class interval {
-    public:
-        double min, max;
+  public:
+    double min, max;
 
-        interval(): min(+infinity), max(-infinity) {} // empty interval
+    interval() : min(+infinity), max(-infinity) {} // Default interval is empty
 
-        interval(double min, double max): min(min), max(max) {}
+    interval(double min, double max) : min(min), max(max) {}
 
-        double size() const {
-            return max - min;
-        }
+    double size() const {
+        return max - min;
+    }
 
-        bool contains(double x) const {
-            return min <= x && x <= max;
-        }
+    bool contains(double x) const {
+        return min <= x && x <= max;
+    }
 
-        bool surrounds(double x) const {
-            return min < x && x < max;
-        }
+    bool surrounds(double x) const {
+        return min < x && x < max;
+    }
 
-        static const interval empty, universe;  
+    static const interval empty, universe;
 };
 
 const interval interval::empty    = interval(+infinity, -infinity);
--- src/rtweekend.h
+++ src/rtweekend.h
@@ -19,7 +19,7 @@ using std::sqrt;
 
 // Constants
 
-const double infinity = std::numeric_limits<double>::infinity();
+// const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
 // Utility Functions
@@ -27,11 +27,5 @@ const double pi = 3.1415926535897932385;
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
-
-// Common Headers
-
-#include "color.h"
-#include "ray.h"
-#include "vec3.h"
 
 #endif
