65fda9848d6b42c838f7cad7dac9893a533736a7
lngu0114 <lngu0114@student.monash.edu>
2024-07-11 20:40:49 +1000

    added glass and camera movement

--- src/camera.h
+++ src/camera.h
@@ -13,6 +13,11 @@ class camera {
         int samples_per_pixel = 10;
         int max_depth = 10;
 
+        double vfov = 90;
+        point3 lookfrom = point3(0, 0, 0);
+        point3 lookat = point3(0, 0, -1);
+        vec3 vup = vec3(0, 1, 0);
+
         void render(const hittable& world) {
             initialise();
             
@@ -40,6 +45,7 @@ class camera {
         point3 pixel00_loc;
         vec3 pixel_delta_u;
         vec3 pixel_delta_v;
+        vec3 u, v, w;
 
         void initialise() {
             // Calculate the image height, and ensure that it's at least 1.
@@ -48,23 +54,29 @@ class camera {
             
             pixel_samples_scale = 1.0 / samples_per_pixel;
 
-            center = point3(0, 0, 0);
+            center = lookfrom;
 
             // Dimension of viewport
-            auto focal_length = 1.0;
-            auto viewport_height = 2.0;
+            auto focal_length = (lookfrom - lookat).length();
+            auto theta = degrees_to_radians(vfov);
+            auto h = tan(theta / 2);
+            auto viewport_height = 2 * h * focal_length;
             auto viewport_width = viewport_height * (double(image_width)/image_height);
 
-            // Vectors for the across horizontal and down the vertical viewport edges
-            auto viewport_u = vec3(viewport_width, 0, 0);
-            auto viewport_v = vec3(0, -viewport_height, 0);
+            w = unit_vector(lookfrom - lookat);
+            u = unit_vector(cross(vup, w));
+            v = cross(w, u);
 
-            // Horizontal and vertical vectors from pixel to pixel (spacing)
+            // Vectors for the across horizontal and down the vertical viewport edges
+            auto viewport_u = viewport_width * u;
+            auto viewport_v = viewport_height * -v;
+
+            // Horizontal and vertical vectors from pixel to pixel (spa cing)
             pixel_delta_u = viewport_u / image_width;
             pixel_delta_v = viewport_v / image_height;
 
             // Calculate the location of the upper left pixel.
-            auto viewport_upper_left = center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+            auto viewport_upper_left = center - (focal_length * w) - viewport_u/2 - viewport_v/2;
             pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
         }
 
